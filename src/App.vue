934feb65eac1b4d0c6fa054882362270cd497dcc
moisesmengo <moisesmengo@live.com>
2020-11-23 15:20:11 -0300

    estilizando home

--- src/App.vue
+++ src/App.vue
@@ -26,9 +26,31 @@ a {
   text-decoration: none;
   color: rgb(36, 48, 59);
 }
+img {
+  max-width: 100%;
+}
 .container {
   max-width: 800px;
   margin: 0 auto;
   padding: 20px;
+}
+
+.v-enter {
+  opacity: 0;
+  transform: translate3d(-20px, 0, 0);
+}
+.v-enter-active {
+  transition: all 0.3s;
+}
+.conteudo {
+  display: grid;
+  grid-template-columns: 1fr minmax(200px, 400px);
+  grid-gap: 30px;
+}
+
+@media screen and (max-width: 500px) {
+  .conteudo {
+    display: block;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="home">
     <div v-if="loading">
-      <Loading/>
+      <Loading />
     </div>
-    <div v-if="api">
-      <h1>Home</h1>
-      <p>{{api}}</p>
-    </div>
+    <transition>
+      <div v-if="api" class="conteudo">
+        <div>
+          <h1>Sobre a {{api.titulo}}</h1>
+          <p>{{api.descricao}}</p>
+          <router-link to="/cursos" tag="button" class="btn-cursos">Cursos</router-link>
+          <div>
+            <h2>Avaliações</h2>
+            <ul>
+              <li v-for="avaliacao in api.avaliacoes" :key="avaliacao.nome">
+                <p>{{avaliacao.nome}}</p>
+                <p>{{avaliacao.descricao}}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img src="../assets/05.png" alt="conhecimento">
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -20,3 +35,24 @@
     },
   };
 </script>
+<style scoped>
+  .btn-cursos {
+    border: none;
+    background-color: #4b8;
+    border-radius: 4px;
+    color: aliceblue;
+    cursor: pointer;
+    font-size: 1rem;
+    margin-bottom: 40px;
+    margin-top: 10px;
+    font-family: "Xanh Mono", monospace;
+    font-weight: bold;
+    padding: 15px 20px;
+    box-shadow: 0 4px 2px rgba(0, 0, 0, .1);
+    transition: filter .5s all;
+  }
+
+  .btn-cursos:hover {
+    filter: brightness(80%);
+  }
+</style>
